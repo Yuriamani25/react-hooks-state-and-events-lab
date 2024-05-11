e40cc073b7c859e3e09459271d6df92684c4a804
YuriAmani <yuriamani7@gmail.com>
2024-05-11 13:57:06 +0300

    react-hooks-state-and-events-lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [tag,setTag] = useState(false)
+
+function handleClick() {
+  setTag((tag) => !tag)
+}
+
+
+
   return (
-    <li className="">
+    <li className={tag ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{tag ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
